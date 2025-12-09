4523bb35b1526be9dd50e1424239b5152254ee76
Radho <radhofanazizi@gmail.com>
2025-12-09 17:19:10 +0700

    sql connect

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 // index.js
+import "dotenv/config";
 import express from "express";
+import pkg from "pg";
+const { Pool } = pkg;
 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: { rejectUnauthorized: false },
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+pool
+  .query("SELECT NOW()")
+  .then((res) => console.log("Postgres connected:", res.rows[0]))
+  .catch((err) => console.error("Postgres connection error:", err));
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Hello World! Postgres time is: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
